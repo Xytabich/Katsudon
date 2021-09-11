13ca1827fc9db90610270df2c99e1b11b3f89a42
Xytabich <xytabich@gmail.com>
2021-09-11 04:39:53 +0300

    field, ctor not supported exceptions

---
+++ Assets/Katsudon/Builder/Exceptions/CtorNotSupportException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Katsudon.Builder.Exceptions
+{
+	[OperationBuilder]
+	public class CtorNotSupportException : IOperationBuider
+	{
+		public int order => 10000;
+
+		bool IOperationBuider.Process(IMethodDescriptor method)
+		{
+			var ctorInfo = method.currentOp.argument as ConstructorInfo;
+			throw new Exception(string.Format("Constructor {0} declared in {1} is not supported by udon", ctorInfo, ctorInfo.DeclaringType));
+		}
+
+		public static void Register(IOperationBuildersRegistry container, IModulesContainer modules)
+		{
+			var builder = new CtorNotSupportException();
+			container.RegisterOpBuilder(OpCodes.Newobj, builder);
+		}
+	}
+}
---
+++ Assets/Katsudon/Builder/Exceptions/FieldNotSupportException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Katsudon.Builder.Exceptions
+{
+	[OperationBuilder]
+	public class FieldNotSupportException : IOperationBuider
+	{
+		public int order => 10000;
+
+		bool IOperationBuider.Process(IMethodDescriptor method)
+		{
+			var fieldInfo = method.currentOp.argument as FieldInfo;
+			throw new Exception(string.Format("Field {0} declared in {1} is not supported by udon", fieldInfo, fieldInfo.DeclaringType));
+		}
+
+		public static void Register(IOperationBuildersRegistry container, IModulesContainer modules)
+		{
+			var builder = new FieldNotSupportException();
+			container.RegisterOpBuilder(OpCodes.Stfld, builder);
+			container.RegisterOpBuilder(OpCodes.Stsfld, builder);
+			container.RegisterOpBuilder(OpCodes.Ldfld, builder);
+			container.RegisterOpBuilder(OpCodes.Ldflda, builder);
+			container.RegisterOpBuilder(OpCodes.Ldsfld, builder);
+			container.RegisterOpBuilder(OpCodes.Ldsflda, builder);
+		}
+	}
+}
