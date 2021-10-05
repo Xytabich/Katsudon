e3f25844424cb4a2c8d1d132d862312a21394019
Xytabich <xytabich@gmail.com>
2021-10-05 21:58:21 +0300

    infinity loop fix

--- Assets/Katsudon/Builder/Utils/ForLoop.cs
+++ Assets/Katsudon/Builder/Utils/ForLoop.cs
@@ -23,6 +23,7 @@ namespace Katsudon.Utility
 			breakLabel = new EmbedAddressLabel();
 
 			method.machine.ApplyLabel(continueLabel);
+			method.machine.BinaryOperatorExtern(BinaryOperator.Addition, indexVariable, method.machine.GetConstVariable((int)1), indexVariable);
 			var condition = method.GetTmpVariable(typeof(bool));
 			method.machine.BinaryOperatorExtern(BinaryOperator.LessThan, indexVariable, lengthVariable, condition);
 			method.machine.AddBranch(condition, breakLabel);
@@ -30,7 +31,6 @@ namespace Katsudon.Utility
 
 		public void Dispose()
 		{
-			method.machine.BinaryOperatorExtern(BinaryOperator.Addition, indexVariable, method.machine.GetConstVariable((int)1), indexVariable);
 			method.machine.AddJump(continueLabel);
 			method.machine.ApplyLabel(breakLabel);
 
@@ -42,7 +42,7 @@ namespace Katsudon.Utility
 		public static ForLoop Array(IMethodDescriptor method, IVariable array, out IVariable index)
 		{
 			var indexVariable = method.GetTmpVariable(typeof(int)).Reserve();
-			method.machine.AddCopy(method.machine.GetConstVariable((int)0), indexVariable);
+			method.machine.AddCopy(method.machine.GetConstVariable((int)-1), indexVariable);
 
 			var length = method.GetTmpVariable(typeof(int)).Reserve();
 			method.machine.AddExtern("SystemArray.__get_Length__SystemInt32", length, array.OwnType());
@@ -69,6 +69,7 @@ namespace Katsudon.Utility
 			breakLabel = new EmbedAddressLabel();
 
 			method.machine.ApplyLabel(continueLabel);
+			method.machine.BinaryOperatorExtern(BinaryOperator.Subtraction, indexVariable, method.machine.GetConstVariable((int)1), indexVariable);
 			var condition = method.GetTmpVariable(typeof(bool));
 			method.machine.BinaryOperatorExtern(BinaryOperator.GreaterThanOrEqual, indexVariable, method.machine.GetConstVariable((int)0), condition);
 			method.machine.AddBranch(condition, breakLabel);
@@ -76,7 +77,6 @@ namespace Katsudon.Utility
 
 		public void Dispose()
 		{
-			method.machine.BinaryOperatorExtern(BinaryOperator.Subtraction, indexVariable, method.machine.GetConstVariable((int)1), indexVariable);
 			method.machine.AddJump(continueLabel);
 			method.machine.ApplyLabel(breakLabel);
 
@@ -88,7 +88,6 @@ namespace Katsudon.Utility
 		{
 			var indexVariable = method.GetTmpVariable(typeof(int)).Reserve();
 			method.machine.AddExtern("SystemArray.__get_Length__SystemInt32", indexVariable, array.OwnType());
-			method.machine.BinaryOperatorExtern(BinaryOperator.Subtraction, indexVariable, method.machine.GetConstVariable((int)1), indexVariable);
 
 			index = indexVariable;
 			return new ReverseForLoop(method, indexVariable);
@@ -99,7 +98,6 @@ namespace Katsudon.Utility
 		{
 			var indexVariable = method.GetTmpVariable(typeof(int)).Reserve();
 			method.machine.AddCopy(length, indexVariable);
-			method.machine.BinaryOperatorExtern(BinaryOperator.Subtraction, indexVariable, method.machine.GetConstVariable((int)1), indexVariable);
 
 			index = indexVariable;
 			return new ReverseForLoop(method, indexVariable);
